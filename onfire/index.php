3c0441007a2b802cd0495e14dacaf31671c348a5
Brian Vestergaard Danielsen <brian.v.danielsen@gmail.com>
2022-12-18 12:22:19 +0100

    Remove unused files

--- onfire/index.php
+++
@@ -1,9 +0,0 @@
-<?php include("../../includes/header.php"); ?>
-<div id="main">
-	<div id="menu">
-		<?php include("../../includes/menu.php"); ?>
-	</div>
-	<div id="content">
-		<h1>ON FIRE!</h1>
-	<div>
-</div>
